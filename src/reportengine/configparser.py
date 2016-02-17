dc6206e492e9428825d0d6fbf39770392de59733
Zahari <zaharid@gmail.com>
2016-02-17 21:49:51 +0100

    Make parse functions check type themselves
    This makes the interface more pleasant to use, and allows to add
    metadata to the funcs and then retrieve them with get_parse_func.

--- src/reportengine/configparser.py
+++ src/reportengine/configparser.py
@@ -6,12 +6,16 @@ Created on Fri Dec 11 15:31:29 2015
 """
 import inspect
 import difflib
+import logging
+import functools
 
 import yaml
 
 from reportengine.dag import DAG
 
+log = logging.getLogger(__name__)
 
+_config_token = 'check_'
 
 class ConfigError(Exception):
 
@@ -48,15 +52,21 @@ class BadInputType(ConfigError, TypeError):
                "is not of type {input_type}.").format(**locals())
         super().__init__(msg)
 
-def element_of(paramname):
+def element_of(paramname, elementname=None):
     def inner(f):
+        nonlocal elementname
+        if elementname is None:
+            if f.__name__.startswith(_config_token):
+                elementname = f.__name__[len(_config_token):]
+
         f._element_of = paramname
+        f._elementname = elementname
         return f
     return inner
 
 def _make_element_of(f):
     def parse_func(self, param:list, **kwargs):
-                return [f(self, elem, **kwargs) for elem in param]
+        return [f(self, elem[f._elementname], **kwargs) for elem in param]
 
     #We replicate the same signature for the kwarg parameters, so that we can
     #use that to build the graph.
@@ -66,39 +76,98 @@ def _make_element_of(f):
     parse_func.__signature__ = inspect.Signature(parameters=params)
     return parse_func
 
+
+def _parse_func(f):
+
+    @functools.wraps(f)
+    def _f(self, val, **kwargs):
+        sig = inspect.signature(f)
+
+        try:
+            first_param = list(sig.parameters.values())[1]
+        except IndexError:
+            raise TypeError(("Parser functiom must have at least one "
+                            "parameter: %s")
+                            % f.__qualname__)
+
+        input_type = first_param.annotation
+        if input_type is not sig.empty:
+            if not isinstance(val, input_type):
+                raise BadInputType(f.__name__, val, input_type)
+
+
+        return f(self, val, **kwargs)
+
+    return _f
+
 class ElementOfResolver(type):
     def __new__(cls, name, bases, attrs):
         newattrs = {}
+        _list_keys = {}
         for attr, f in attrs.items():
             if hasattr(f, '_element_of'):
-                newattr = 'check_' + f._element_of
+                newattr = _config_token + f._element_of
                 if newattr in attrs:
                     raise ValueError("Cannot construct {newattr} from "
                                      "'_element_of' {attr} because it is "
                                      "already declared.")
 
-                newattrs[newattr] = _make_element_of(f)
+                #We have to apply parse func in here as well.
+                newattrs[newattr] = _make_element_of(_parse_func(f))
+                _list_keys[f._element_of] = f._elementname
+
+        newattrs['_list_keys'] = _list_keys
 
         attrs = {**newattrs, **attrs}
         return super().__new__(cls, name, bases, attrs)
 
+class AutoTypeCheck(type):
+    def __new__(cls, name, bases, attrs):
+        for k,v in attrs.items():
+            if k.startswith(_config_token):
+                attrs[k] = _parse_func(v)
+        return super().__new__(cls, name, bases, attrs)
 
-class Config(metaclass=ElementOfResolver):
+class ConfigMetaClass(ElementOfResolver, AutoTypeCheck):
+    pass
+
+class Config(metaclass=ConfigMetaClass):
 
     def __init__(self, input_params, environment=None):
         self.environment = environment
         self.input_params = input_params
 
+        self._transform_input()
         self.params = self.process_params(input_params)
 
+    def _transform_input(self, parent=None):
+        if parent is None:
+            parent = self.input_params
+        for key, value in parent.items():
+            if isinstance(value, dict):
+                self._transform_input(value)
+
+            if isinstance(value, list):
+                if key in self._list_keys:
+                    inner_key = self._list_keys[key]
+                    log.debug("Transforming input list %s to add key %s" %
+                               (key,inner_key))
+                    parent[key] = [{inner_key: x} for x in value]
+
+                for elem in value:
+                    if isinstance(elem, dict):
+                        self._transform_input(elem)
 
     def get_parse_func(self, param):
-        func_name = "check_" + param
-        return getattr(self, func_name, None)
+        func_name = _config_token + param
+        try:
+            return getattr(self, func_name)
+        except AttributeError:
+            return lambda x : x
 
-    def make_graph(self):
+    def make_graph(self, params):
         g = DAG()
-        for param in self.input_params:
+        for param in params:
             f = self.get_parse_func(param)
             reqs = set()
             if f:
@@ -111,38 +180,19 @@ class Config(metaclass=ElementOfResolver):
 
         return g
 
-
-
-    def process_params(self, input_params):
+    def process_params(self, input_params=None):
+        if input_params is None:
+            input_params = self.input_params
         params = {}
-        g = self.make_graph()
+        g = self.make_graph(input_params)
         for node in g.topological_iter():
             param = node.value
             kwargs = {inp.value:params[inp.value] for inp in node.inputs}
-            val = self.parse_param(param, input_params[param], **kwargs)
+            parse_func = self.get_parse_func(param)
+            val = parse_func(input_params[param],**kwargs)
             params[param] = val
         return params
 
-    def parse_param(self, param, val, **kwargs):
-
-        parse_func = self.get_parse_func(param)
-        if parse_func is None:
-            return val
-
-        sig = inspect.signature(parse_func)
-        try:
-            first_param = next(iter(sig.parameters.values()))
-        except StopIteration:
-            raise TypeError("Parser functiom must have one parameter: %s"
-                            % parse_func.__qualname__)
-
-        input_type = first_param.annotation
-        if input_type is not sig.empty:
-            if not isinstance(val, input_type):
-                raise BadInputType(param, val, input_type)
-
-        result = parse_func(val, **kwargs)
-        return result
 
     def __getitem__(self, item):
         return self.params[item]
--- src/reportengine/tests/test_configparser.py
+++ src/reportengine/tests/test_configparser.py
@@ -7,23 +7,28 @@ Created on Mon Jan 18 11:09:28 2016
 
 import unittest
 
-from reportengine.configparser import Config ,BadInputType
+from reportengine.configparser import Config ,BadInputType, element_of
 
 class ExampleConfig(Config):
-    
-    @staticmethod
-    def parse_three(number:int):
+
+
+    @element_of("fours")
+    def check_four(self, number:int):
+        return 4
+
+    def check_three(self, number:int):
         return number
-    
-    
+
+
 
 class TestConfig(unittest.TestCase):
-    
+
     def test_simple_input(self):
         inp = {'one':1, 'two':2}
         c = ExampleConfig(inp)
+        c.process_params()
         self.assertEqual(c.params, inp)
-    
+
     def test_types(self):
         inp = {'three': 'spam'}
         with self.assertRaises(BadInputType):
@@ -31,13 +36,25 @@ class TestConfig(unittest.TestCase):
         inp = {'three' : 3}
         c = ExampleConfig(inp)
         self.assertEqual(c.params, inp)
-    
+
     def test_interface(self):
         inp = {'one':1, 'two':2, 'three': 3}
         c = ExampleConfig(inp)
         self.assertTrue('one' in c)
         self.assertEqual(c['three'], 3)
         self.assertEqual(len(c), 3)
+
+    def test_transform(self):
+
+        inp = {'fours': [4,4,4,4,4]}
+        ExampleConfig(inp)
+        self.assertEqual(inp, {'fours': [{'four': 4}, {'four': 4},
+                                         {'four': 4}, {'four': 4},
+                                         {'four': 4}]})
+        inp = {'fours': [4,'x']}
+        with self.assertRaises(BadInputType):
+            ExampleConfig(inp)
+
 
 if __name__ =='__main__':
     unittest.main()
