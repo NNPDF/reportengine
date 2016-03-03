10e864bf623f7fc920ea14e621935fc05c87e4fb
Zahari <zaharid@gmail.com>
2016-03-03 19:08:25 +0100

    Add test for default and required config arguments

--- src/reportengine/tests/test_configparser.py
+++ src/reportengine/tests/test_configparser.py
@@ -7,21 +7,27 @@ Created on Mon Jan 18 11:09:28 2016
 
 import unittest
 
-from reportengine.configparser import Config ,BadInputType, element_of, named_element_of
+from reportengine.configparser import Config, BadInputType, element_of, named_element_of, ConfigError
 
 class ExampleConfig(Config):
+
+    def check_y(self, number):
+        return number
 
 
     @element_of("fours")
     def check_four(self, number:int):
         return 4
-    
+
     @named_element_of("fives")
     def check_five(self, number:int):
         return 5
 
     def check_three(self, number:int):
         return number
+
+    def check_sum(self, sum, y, three=3, four=4):
+        return three + four + y
 
 
 
@@ -48,6 +54,24 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(c['three'], 3)
         self.assertEqual(len(c), 3)
 
+    def test_graph(self):
+        inp = {'sum':None, 'three': 3, 'y': 0}
+        c = ExampleConfig(inp)
+        self.assertEqual(c.params, {'sum':7, 'three':3, 'y':0})
+
+        inp = {'sum':None, 'three': 3, 'four':10, 'y':0}
+        c = ExampleConfig(inp)
+        self.assertEqual(c.params, {'sum':7, 'three':3, 'four':4, 'y':0})
+
+        inp = {'sum':None, 'three': 3, 'four':10}
+        with self.assertRaises(ConfigError):
+            c = ExampleConfig(inp)
+
+        inp = {'sum':None, 'three': 3, 'four':10}
+
+        with self.assertRaises(ConfigError):
+            c = ExampleConfig(inp)
+
     def test_transform(self):
 
         inp = {'fours': [4,4,4,4,4]}
@@ -59,11 +83,11 @@ class TestConfig(unittest.TestCase):
         inp = {'fours': [4,'x']}
         with self.assertRaises(BadInputType):
             ExampleConfig(inp)
-            
+
         inp = {'fours': "patata"}
         with self.assertRaises(BadInputType):
             ExampleConfig(inp)
-        
+
         inp = {'fives': {'f1': 55, 'f2':555}}
         r = ExampleConfig(inp).params['fives']
         self.assertEqual(r, {'f1': 5 ,'f2' : 5})
