34d4fb03ed69f893106c65f07ecfe6e23499133a
Zahari <zaharid@gmail.com>
2016-04-19 18:42:01 +0200

    Handle figure_formats better
    This way we can set them at any time, and not only on event creation.

--- src/reportengine/environment.py
+++ src/reportengine/environment.py
@@ -12,7 +12,8 @@ log = logging.getLogger(__name__)
 
 class EnvironmentError_(Exception): pass
 
-available_figure_formats = {'eps': 'Encapsulated Postscript',
+available_figure_formats = {
+ 'eps': 'Encapsulated Postscript',
  'jpeg': 'Joint Photographic Experts Group',
  'jpg': 'Joint Photographic Experts Group',
  'pdf': 'Portable Document Format',
@@ -24,12 +25,35 @@ available_figure_formats = {'eps': 'Encapsulated Postscript',
  'svg': 'Scalable Vector Graphics',
  'svgz': 'Scalable Vector Graphics',
  'tif': 'Tagged Image File Format',
- 'tiff': 'Tagged Image File Format'}
+ 'tiff': 'Tagged Image File Format'
+}
 
 class Environment:
     def __init__(self, *, output_path, figure_formats=('pdf',),
                  default_figure_format=None):
         self.output_path = pathlib.Path(output_path).absolute()
+        self.figure_formats = figure_formats
+
+
+        self._default_figure_format = default_figure_format
+
+    @property
+    def figure_formats(self):
+        return self._figure_formats
+
+    @property
+    def default_figure_format(self):
+        if self._default_figure_format is None:
+            return self.figure_formats[0]
+        else:
+            return self._default_figure_format
+
+    @default_figure_format.setter
+    def default_figure_format(self, fmt):
+        self._default_figure_format = fmt
+
+    @figure_formats.setter
+    def figure_formats(self, figure_formats):
         if isinstance(figure_formats, str):
             figure_formats = (figure_formats,)
         if not isinstance(figure_formats, Sequence):
@@ -41,10 +65,7 @@ class Environment:
             raise EnvironmentError_("The following are not valid figure"
                     "formats %s: It must be one of %s" % (bad_formats,
                                                      available_figure_formats))
-        self.figure_formats = figure_formats
-        if default_figure_format is None:
-            default_figure_format = figure_formats[0]
-        self.default_figure_format = default_figure_format
+        self._figure_formats = figure_formats
 
     def init_output(self):
         if self.output_path.exists():
