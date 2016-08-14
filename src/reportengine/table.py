9781140932c314c5668af538a3757885abfad3f6
Zahari <zaharid@gmail.com>
2016-08-14 14:00:54 +0200

    Save tables using the new technology

--- src/reportengine/table.py
+++ src/reportengine/table.py
@@ -15,34 +15,47 @@ Created on Mon May 16 11:36:13 2016
 
 @author: Zahari Kassabov
 """
+import logging
+
 from reportengine.formattingtools import spec_to_nice_name
 from reportengine.utils import add_highlight
 
 __all__ = ('table', 'tablegen')
 
+log = logging.getLogger(__name__)
 
-def savetable(df, environment, spec, namespace, graph, *, suffix=''):
-    """Final action to save figures, with a nice filename"""
-
-    name = spec_to_nice_name(namespace, spec, suffix)
+def prepare_path(*, spec, namespace,environment, **kwargs):
+    name = spec_to_nice_name(namespace, spec)
     path = environment.table_folder / (name + '.csv')
+    return {'path': path}
+
+def savetable(df, path):
+    """Final action to save figures, with a nice filename"""
+    log.debug("Writing table %s" % path)
     df.to_csv(str(path), sep='\t', na_rep='nan')
 
-def savetablelist(figures, environment, spec, namespace, graph):
+def savetablelist(dfs, path):
     """Final action to save lists of figures. It adds a numerical index as
     a suffix, for each figure in the generator."""
 
-    for i, fig in enumerate(figures):
-        savetable(fig, environment, spec, namespace, graph, suffix=str(i))
+    for i, df in enumerate(dfs):
+        if isinstance(df, tuple):
+            df, suffix = df
+        else:
+            suffix = str(i)
+        tb_path = path.with_name(path.stem + suffix, path.suffix)
+        savetable(df, tb_path)
 
 @add_highlight
 def table(f):
     """Save the resulting table as a tab separated csv file after
     it is generated"""
+    f.prepare = prepare_path
     f.final_action = savetable
     return f
 @add_highlight
 def tablegen(f):
     """Save each table of the generator. See ``table``."""
+    f.prepare = prepare_path
     f.final_action = savetablelist
     return f
