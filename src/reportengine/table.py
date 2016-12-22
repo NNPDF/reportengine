e9c3acb8df34338c5eddc78150d84c3e7f8b2f09
Zahari <zaharid@gmail.com>
2016-12-22 21:49:59 +0100

    Enable tables in report
    I'm not sure that wrapping them is a great idea, as contrary to the
    figures, we possibly want to use tables both as inputs and outputs.
    Let's see how well this works in practice.
    
    Otherwise yet another protocol could be added to specify which is the
    "return value" and which is the "saved value", but maybe it has to
    wait until some reorganization of the namespaces.
    
    Also add pandas as an explicit dependency (doesn't matter too much in
    practice).

--- src/reportengine/table.py
+++ src/reportengine/table.py
@@ -17,12 +17,27 @@ Created on Mon May 16 11:36:13 2016
 """
 import logging
 
+#TODO: Get rid of explicit pandas dependency here?
+import pandas as pd
+
 from reportengine.formattingtools import spec_to_nice_name
 from reportengine.utils import add_highlight
 
 __all__ = ('table', 'tablegen')
 
 log = logging.getLogger(__name__)
+
+class Table:
+    def __init__(self, dataframe, description=None):
+        self.dataframe = dataframe
+
+    @property
+    def as_markdown(self):
+        with pd.option_context('display.max_colwidth', -1):
+            return self.dataframe.to_html(escape = False)
+
+
+
 
 def prepare_path(*, spec, namespace,environment, **kwargs):
     name = spec_to_nice_name(namespace, spec)
@@ -33,10 +48,13 @@ def savetable(df, path):
     """Final action to save figures, with a nice filename"""
     log.debug("Writing table %s" % path)
     df.to_csv(str(path), sep='\t', na_rep='nan')
+    return Table(df)
 
 def savetablelist(dfs, path):
     """Final action to save lists of figures. It adds a numerical index as
     a suffix, for each figure in the generator."""
+
+    res = []
 
     for i, df in enumerate(dfs):
         if isinstance(df, tuple):
@@ -45,6 +63,10 @@ def savetablelist(dfs, path):
             suffix = str(i)
         tb_path = path.with_name(path.stem + suffix, path.suffix)
         savetable(df, tb_path)
+        res.append(Table[df])
+    return df
+
+
 
 @add_highlight
 def table(f):
