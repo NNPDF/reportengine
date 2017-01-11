f5c11595433c7eeb7841a7de55ed1dff2806dca1
Zahari <zaharid@gmail.com>
2017-01-11 15:04:09 +0100

    Add failing test
    The datasets should be resolved differently depending on whether we
    have cuts or not.

---
+++ src/reportengine/tests/test_app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 10:00:19 2016
+
+@author: Zahari Kassabov
+"""
+import unittest
+
+from reportengine import app
+
+class TestApp(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
--- src/reportengine/tests/test_complexinput.py
+++ src/reportengine/tests/test_complexinput.py
@@ -6,9 +6,16 @@ Created on Fri Mar  4 21:09:40 2016
 """
 import unittest
 
-from reportengine import namespaces, configparser, utils
+from reportengine import namespaces, configparser, utils, resourcebuilder
+from reportengine.resourcebuilder import FuzzyTarget
 
-inp = {'pdfsets': ['a', 'b'], 'theories': [1,2], 'datasets': ['d1', 'd2']}
+inp = {'pdfsets': ['a', 'b'],
+       'theories': [1,2],
+       'datasets': ['d1', 'd2'],
+       'use_cuts': False,
+       'cuts': {'use_cuts':True},
+       'nocuts': {'use_cuts':False},
+       }
 
 class Config(configparser.Config):
     @configparser.element_of('pdfsets')
@@ -20,8 +27,8 @@ class Config(configparser.Config):
         return 'th ' + str(theory)
 
     @configparser.element_of('datasets')
-    def parse_dataset(self, ds,  theory):
-        return 'ds: {ds} (theory: {theory})'.format(**locals())
+    def parse_dataset(self, ds,  theory, use_cuts):
+        return 'ds: {ds} (theory: {theory}, cuts: {use_cuts})'.format(**locals())
 
 
 class TestSpec(unittest.TestCase):
@@ -32,16 +39,31 @@ class TestSpec(unittest.TestCase):
         specs = c.process_fuzzyspec(spec, ns=ns)
         self.assertEqual(len(specs), 8)
         datasets = [
-              'ds: d1 (theory: th 1)',
-              'ds: d2 (theory: th 1)',
-              'ds: d1 (theory: th 2)',
-              'ds: d2 (theory: th 2)',
-              'ds: d1 (theory: th 1)',
-              'ds: d2 (theory: th 1)',
-              'ds: d1 (theory: th 2)',
-              'ds: d2 (theory: th 2)']
+              'ds: d1 (theory: th 1, cuts: False)',
+              'ds: d2 (theory: th 1, cuts: False)',
+              'ds: d1 (theory: th 2, cuts: False)',
+              'ds: d2 (theory: th 2, cuts: False)',
+              'ds: d1 (theory: th 1, cuts: False)',
+              'ds: d2 (theory: th 1, cuts: False)',
+              'ds: d1 (theory: th 2, cuts: False)',
+              'ds: d2 (theory: th 2, cuts: False)']
         for spec, ds in zip(specs, datasets):
             self.assertEqual(namespaces.resolve(ns, spec)['dataset'], ds)
+
+    def test_nsspec(self):
+        c = Config(inp)
+        spec = ('pdfsets', 'theories', 'datasets')
+        targets = [
+                   FuzzyTarget('dataset', spec+(), (), ()),
+                   FuzzyTarget('dataset', spec+('cuts',), (), ()),
+                   FuzzyTarget('dataset', spec+('nocuts',), (), ()),
+                  ]
+        builder = resourcebuilder.ResourceBuilder(c, (), targets)
+        builder.resolve_fuzzytargets()
+        builder.execute_sequential()
+        ns = namespaces.resolve(builder.rootns,
+                  (('pdfsets',0), ('theories', 0), ('datasets', 0), 'cuts',))
+        assert(ns['dataset']=="ds: d1 (theory: th 1, cuts: True)" )
 
 
 if __name__ == '__main__':
