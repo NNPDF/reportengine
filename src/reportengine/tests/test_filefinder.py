27d760ab1719321363817b04559d12a3b2bf89cf
Zahari <zaharid@gmail.com>
2017-03-28 14:19:14 +0200

    Add file finder API
    This is mostly to replace the awful (for our purposes at least) API of
    the jinja2 loaders. It will now become easier to copy the templates to
    the input folder in such a way that the input config is fully
    reproducible by reruning it.
    
    Also, it is no longer needed to pass around open files sooner than
    they are needed.
    
    The new functionality is mainly to be used internally and has 100%
    test coverage.

---
+++ src/reportengine/tests/test_filefinder.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 12:32:31 2017
+
+@author: zah
+"""
+import pathlib
+import shutil
+
+import pytest
+
+import reportengine
+from reportengine.filefinder import AbsFinder, FallbackFinder, ModuleFinder
+
+def test_fileloader(tmpdir):
+    #TODO: PY36
+    p = pathlib.Path(str(tmpdir))
+    patata = (p/'patata')
+    patata.touch()
+    d = p/'dir'
+    d.mkdir()
+    (d/'file').touch()
+
+
+    f = FallbackFinder([str(tmpdir), AbsFinder()])
+    assert f.find('patata') == (p, 'patata')
+    assert f.find('dir/file') == (p, 'dir/file')
+    assert f.find('/dev/null') == (pathlib.Path('/dev'), 'null')
+
+    assert set(f.hint_files()) == {(p, 'patata'), (p, 'dir')}
+
+    with pytest.raises(ValueError):
+        FallbackFinder([patata])
+    #TODO: PY36
+    shutil.rmtree(str(p))
+    with pytest.raises(FileNotFoundError):
+        f.find('patata')
+
+    mf = ModuleFinder(reportengine)
+    assert mf.find('__init__.py')
+
+
+
+
+
+
