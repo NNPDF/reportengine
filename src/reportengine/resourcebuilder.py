a6a677841ae74130deb5f84fa8af6698d1ede465
Zahari <zaharid@gmail.com>
2017-06-30 13:20:31 +0200

    Fix MAX_WORKER_PROCESSES

--- src/reportengine/resourcebuilder.py
+++ src/reportengine/resourcebuilder.py
@@ -219,7 +219,15 @@ class ResourceExecutor():
 
     def execute_parallel(self):
         if 'MAX_WORKER_PROCESSES' in os.environ:
-            curio.workers.MAX_WORKER_PROCESSES = os.environ['MAX_WORKER_PROCESSES']
+            try:
+                workers = int(os.environ['MAX_WORKER_PROCESSES'])
+            except Exception as e:
+                log.warning('Could not interpret the value of the environment variable MAX_WORKER_PROCESSES as an integer. Ignoring it.')
+            if workers>=1:
+                curio.workers.MAX_WORKER_PROCESSES
+            else:
+                log.warning('The environment variable MAX_WORKER_PROCESSES must be greater >=1. Ignoring it.')
+
         deps = self.graph.dependency_resolver()
         #https://github.com/dabeaz/curio/issues/72
         kernel = curio.Kernel()
