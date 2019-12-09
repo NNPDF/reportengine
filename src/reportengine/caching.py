d5c62dff6bd1fe7aad05174b27db2ab2247f9f21
siranipour <si292@cam.ac.uk>
2019-12-09 17:43:41 +0000

    Accounting for illegal characters such as /

--- src/reportengine/caching.py
+++ src/reportengine/caching.py
@@ -8,7 +8,8 @@ def cache_to_file(directory: str="/tmp"):
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            suffix = pathlib.Path(str(hash(func.__name__ + "(" + str(args) + ", "+ str(kwargs) + ")")))
+            suffix = func.__name__ + "(" + str(args) + ", "+ str(kwargs) + ")"
+            suffix = pathlib.Path(suffix.replace("/", ""))
             cache_file = path / suffix
 
             if cache_file.exists():
