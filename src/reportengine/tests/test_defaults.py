fcf0ceb37d59086ce7f3923148291437fec465ca
Zahari <zaharid@gmail.com>
2020-02-28 10:52:46 +0000

    Add tests for lock files
    This should cover all the cases, although some stateful API might be
    able to test that the lock files.stateful API might be able to test that
    the lock files.

---
+++ src/reportengine/tests/test_defaults.py
@@ -0,0 +1,45 @@
+from reportengine import configparser
+from reportengine import api
+
+FILTER_DEFAULTS = {'highpass': [8, 9, 10], 'lowpass': [1, 2, 3]}
+
+
+class TestConfig(configparser.Config):
+    @configparser.record_from_defaults
+    def parse_filter_defaults(self, spec):
+        return spec
+
+    def load_default_filter_defaults(self, spec):
+        return FILTER_DEFAULTS[spec]
+
+    def produce_actual_filter(
+        self, filter_defaults, filter_defaults_recorded_spec_=None
+    ):
+        if filter_defaults_recorded_spec_ is not None:
+            return filter_defaults_recorded_spec_[filter_defaults]
+        print(filter_defaults)
+        return self.load_default_filter_defaults(filter_defaults)
+
+
+class Providers:
+    def data(self, actual_filter):
+        return actual_filter
+
+
+class Env:
+    pass
+
+
+TestAPI = api.API([Providers()], TestConfig, Env)
+
+
+def test_defaults():
+    assert TestAPI.data(filter_defaults='lowpass') == [1, 2, 3]
+    assert TestAPI.data(
+        filter_defaults='lowpass',
+        filter_defaults_recorded_spec_={'lowpass': [10, 20, 30]},
+    ) == [10, 20, 30]
+    assert TestAPI.data(
+        filter_defaults='lowpass',
+        filter_defaults_recorded_spec_={'highpass': [10, 20, 30]},
+    ) == [1, 2, 3]
