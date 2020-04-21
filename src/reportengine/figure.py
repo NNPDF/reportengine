f049d45777d386e1e0cf2eb63f59df5913be2222
Cameron Voisey <voisey@hep.phy.cam.ac.uk>
2020-04-21 10:24:26 +0100

    Tidy up savefiglist

--- src/reportengine/figure.py
+++ src/reportengine/figure.py
@@ -93,11 +93,16 @@ def savefiglist(figures, paths, output):
             suffix = str(i)
         suffix = normalize_name(suffix)
         p_base = [paths[i].relative_to(output) for i in range(len(paths))]
-        p_full = [str(p.with_name('_'.join((p.stem, suffix)) + p.suffix)) for p in p_base]
+        p_full = [
+            str(p.with_name('_'.join((p.stem, suffix)) + p.suffix)) for p in p_base
+        ]
         ref = savefig(fig, paths=paths, output=output, suffix=suffix)
-        html = f'<p style="float: left; text-align: center; width: 50%"><img src={p_full[0]} style="width: 100%"><a href={p_full[0]}>.png</a> <a href={p_full[1]}>.pdf</a></p>'
-        if i%2==1:
-            html += '<br style="clear:both" />'
+        html = f"""<p style="float: left; text-align: center; width: 50%">
+                   <img src={p_full[0]} style="width: 100%">
+                   <a href={p_full[0]}>.png</a> <a href={p_full[1]}>.pdf</a>
+                   </p>"""
+        if i % 2 == 1:
+            html += '<br style="clear:both"/>'
         res.append(html)
     return res
 
