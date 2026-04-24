72662994f4aadc9295b75f704a3c2bbffecf6ae7
Ramon Winterhalder <35335120+ramonpeter@users.noreply.github.com>
2026-04-24 13:19:34 +0200

    Handle zero or non-finite error in __str__ method
    Relevant fix when working with Bayesian Neural networks 
    which by construction gets std=0 for some validphys quantities

--- src/reportengine/floatformatting.py
+++ src/reportengine/floatformatting.py
@@ -83,5 +83,8 @@ class ValueErrorTuple(NamedTuple):
     value: numbers.Real
     error: numbers.Real
     def __str__(self):
+        if self.error == 0 or not np.isfinite(np.log10(self.error)):
+            value = int(self.value) if self.value == int(self.value) else round(self.value, 4)
+            return f'{value}±0'
         valstr, errstr = format_value_error(self.value, self.error)
         return f'{valstr}±{errstr}'
